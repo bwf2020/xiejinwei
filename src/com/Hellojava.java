090d5f49ac5bc27d7cb0e042e23a91cd0b53036f
bwf <xiejinwei@xiejinwei.com>
2021-04-07 11:48:18 +0800

    git eclippde

--- src/com/Hellojava.java
+++ src/com/Hellojava.java
@@ -1,9 +1,38 @@
 package com;
 
+import java.util.Scanner;
+
 public class Hellojava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println('a');
+		System.out.println('A');
+		System.out.println('中');
+		System.out.println('a'+2);
+		System.out.println('A'+2);
+		System.out.println("'");
+		System.out.println("\'");
+		System.out.println("\"");
+		System.out.println("\\");
+		System.out.println("\ucafe");
+		System.out.println("\u0035");
+		Scanner sc=new Scanner(System.in);
+		
+		/*
+		 * 使用scanner类来接受用户输入数据
+		 * 下面需要定义强类型string（相当于一个返回值）
+		 */
+			System.out.println("请输入信息");
+		
+		String str=sc.next();
+		//这里输入的信息会在控制台显示一遍
+		System.out.println("输入的信息是："+str);
+		System.out.println("结束");
+		sc.close();//释放资源
+		
+		
+		
 
 	}
 
---
+++ src/com/test.java
@@ -0,0 +1,31 @@
+package com;
+
+public class test {
+	
+	 static int num=222;
+	 static int y;
+	 static boolean u;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=100;
+		byte b=20;//注意不能越界
+		long d;
+		System.out.println(a);
+		System.out.println(b);
+		d=300;
+		System.out.println(d);
+		int a1,a2,a3;
+		a1=1;// 在使用之前必须初始化
+		System.out.println(a1);
+		System.out.println(num);
+		System.out.println(y);
+		System.out.println(u);
+		a2=2;
+		a3=3;
+		System.out.println(a2);
+		System.out.println(a3);
+
+	}
+
+}
